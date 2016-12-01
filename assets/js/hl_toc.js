956857ca307acdfe67111a50381ef7bc591b60b4
Li Xinyang <lixinyang1026@gmail.com>
2016-12-01 15:44:01 +0800

    Initial commit

---
+++ assets/js/hl_toc.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+    var DAY_IN_MILLISECONDS  = 1000 * 3600 * 24;
+    var HOME_PAGE_URL        = 'http://challenge.li-xinyang.com/';
+    var HOME_PAGE_URL_SECURE = 'https://challenge.li-xinyang.com/';
+    var START_DATE           = new Date('11/10/2016');
+
+    /**
+     * Append elapsed day count label to home page under mountain illustration
+     */
+    function appendElapsedDayLabel() {
+        var url = document.URL;
+        if (url == HOME_PAGE_URL_SECURE || url == HOME_PAGE_URL) {
+            var now = new Date();
+
+            var diff = Math.abs(now.getTime() - START_DATE.getTime());
+            var diffDays = Math.ceil(diff / DAY_IN_MILLISECONDS);
+
+            var label = '<p id="day-count">Day ' + diffDays + '</p>';
+            $('.figure').first().append(label);
+        }
+    }
+
+    appendElapsedDayLabel();
+});
---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -o errexit
+
+# Convert from Org to HTML
+emacs README.org --batch -f org-html-export-to-html --kill
+mv README.html index.html
+
+for dir in */; do
+    full_path="$dir"/README.org
+    emacs $full_path --batch -f org-html-export-to-html --kill
+done
