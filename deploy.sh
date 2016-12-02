50f69fbe331d1ea9dc476afc7b192ef5e9c8ba51
Li Xinyang <lixinyang1026@gmail.com>
2016-12-02 13:20:07 +0800

    Update at 2016-12-02 05:20:06 UTC

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ set -o errexit
 emacs README.org --batch -f org-html-export-to-html --kill
 mv README.html index.html
 
-for dir in */; do
+for dir in */*/; do
     full_path="$dir"/README.org
     emacs $full_path --batch -f org-html-export-to-html --kill
 done
